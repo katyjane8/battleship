b53d547305a8793cc47bba08c21eed76d714df18
katyjane8 <katy.jane8@gmail.com>
2017-09-02 12:51:27 -0600

    welcome message successful

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,20 +1,14 @@
 require 'colorize'
+
 module Messages
 
   def welcome_message
-    message = "Welcome to BATTLESHIP".blue.bold.underline
-    message += "\n\nWould you like to "
-    message += "(p)".green
-    message += "lay, read the "
-    message += "(i)".yellow
-    message += "nstructions, or "
-    message += "(q)".red
-    message += "uit?\n"
-    message
+    "Welcome to BATTLESHIP\n\nWould you like to play (p), read the instructions (i), or quit(q)?\n"
+
   end
 
-  def get_user_input(input = gets.chomp)
-   input
-  end
-  
+  # def get_user_input(input = gets.chomp)
+  #  input
+  # end
+
 end
