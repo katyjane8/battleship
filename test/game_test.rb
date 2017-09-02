c1a613496d055c9e4c84acb896c5a6d970ba5e2b
katyjane8 <katy.jane8@gmail.com>
2017-09-02 15:57:06 -0600

    successful rakefile

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'minitest/spec'
 require './lib/game'
 
 class GameTest < MiniTest::Test
@@ -18,6 +19,20 @@ class GameTest < MiniTest::Test
 
   def test_game_can_get_user_input
     game = Game.new
-    
+
+    assert_equal "", game.get_user_input
+  end
+
+  def test_player_option_p
+    game = Game.new
+
+    assert_equal "p", game.begin_game
+  end
+
+  def test_game_can_begin
+    game = Game.new
+
+    assert_equal "Dave", game.begin_game
+    assert_equal "Hal", game.begin_game
   end
 end
