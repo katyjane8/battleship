c6abe123448dde4aaec996830662000edf29c471
katyjane8 <katy.jane8@gmail.com>
2017-09-02 12:50:26 -0600

    successful game and welcome message test

--- test/game_test.rb
+++ test/game_test.rb
@@ -6,11 +6,13 @@ require '../lib/game'
 class GameTest < MiniTest::Test
   def test_game_exists
     game = Game.new
+
     assert_instance_of Game, game
   end
 
-  def test_run_displays_welcome_message
+  def test_display_welcome_message_after_run
     game = Game.new
-    assert_equal ,
+
+    assert_equal "Welcome to BATTLESHIP\n\nWould you like to play (p), read the instructions (i), or quit(q)?\n", game.welcome_message
   end
 end
