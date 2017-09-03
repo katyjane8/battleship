35bfabe719c59460f6bf5c7870798613ad18b1b4
katyjane8 <katy.jane8@gmail.com>
2017-09-03 15:46:00 -0600

    board test updated to get rid of node tests. exists, ship placed passing

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,19 +1,28 @@
+gem 'minitest'
 require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/board'
+require './lib/twoship'
 
 class BoardTest < MiniTest::Test
-  def test_board_exists
-    board = Board.new
 
-    assert_instance_of Board, board
+  def setup
+    @new_board = Board.new
   end
 
-  def test_board_size
-    board = Board.new
+  def test_board_exists
+    assert_instance_of Board, @new_board
+  end
 
-    assert_equal 4, board.size
+  def test_two_ship_can_be_placed
+    @new_board.place_two_ship("A1", "A2")
+    assert_equal ["A1", "A2"], @new_board.two_ship
+  end
+
+  def test_board_is_empty
+    skip
+    assert_equal ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"], @new_board.create_empty_spaces
   end
 
 end
